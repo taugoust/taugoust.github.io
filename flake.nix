f064d0d08f1f31ffdac6e7bfed246d011ece6bb2
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2026-03-02 11:27:44 +0100

    Fixup flake formatting

--- flake.nix
+++ flake.nix
@@ -1,66 +1,75 @@
 {
-	description = "Personal Website";
+  description = "Personal Website";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-		utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
 
-		hugo-poison-theme = {
-			url = "github:lukeorth/poison";
-			flake = false;
-		};
-	};
+    hugo-poison-theme = {
+      url = "github:lukeorth/poison";
+      flake = false;
+    };
+  };
 
-	outputs = { self, nixpkgs, utils, hugo-poison-theme, ...}:
-	utils.lib.eachDefaultSystem (system:
-	let
-		pkgs = import nixpkgs {
-			inherit system;
-		};
-	in rec {
-		packages = rec {
-			published = pkgs.stdenv.mkDerivation {
-				name = "taugoust.com";
-				src = self;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      hugo-poison-theme,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        packages = rec {
+          published = pkgs.stdenv.mkDerivation {
+            name = "taugoust.com";
+            src = self;
 
-				buildPhase = ''
-					mkdir -p themes/poison
-					cp -r ${hugo-poison-theme}/. themes/poison
-					${pkgs.hugo}/bin/hugo
-				'';
+            buildPhase = ''
+              mkdir -p themes/poison
+              cp -r ${hugo-poison-theme}/. themes/poison
+              ${pkgs.hugo}/bin/hugo
+            '';
 
-				installPhase = ''
-					cp -r public $out
-					cp -r themes $out
-					cp hugo.toml $out
-				'';
-			};
+            installPhase = ''
+              cp -r public $out
+              cp -r themes $out
+              cp hugo.toml $out
+            '';
+          };
 
-			drafts = published.overrideAttrs (old: {
-				buildPhase = ''
-					mkdir -p themes/poison
-					cp -r ${hugo-poison-theme}/. themes/poison
-					${pkgs.hugo}/bin/hugo -D
-				'';
-			});
+          drafts = published.overrideAttrs (old: {
+            buildPhase = ''
+              mkdir -p themes/poison
+              cp -r ${hugo-poison-theme}/. themes/poison
+              ${pkgs.hugo}/bin/hugo -D
+            '';
+          });
 
-			default = drafts;
-		};
+          default = drafts;
+        };
 
-		apps.default = utils.lib.mkApp {
-			drv = pkgs.writeShellScriptBin "pages" "${pkgs.python312}/bin/python3 -m http.server 8000 -d ${packages.default}";
-		};
+        apps.default = utils.lib.mkApp {
+          drv = pkgs.writeShellScriptBin "pages" "${pkgs.python312}/bin/python3 -m http.server 8000 -d ${packages.default}";
+        };
 
-		devShells.default = pkgs.mkShellNoCC {
-			buildInputs = [ pkgs.hugo ];
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = [ pkgs.hugo ];
 
-			# Link theme
-			shellHook = ''
-				mkdir -p themes
-				ln -snf "${hugo-poison-theme}" themes/poison
-			'';
-		};
-	});
+          # Link theme
+          shellHook = ''
+            mkdir -p themes
+            ln -snf "${hugo-poison-theme}" themes/poison
+          '';
+        };
+      }
+    );
 }
-
